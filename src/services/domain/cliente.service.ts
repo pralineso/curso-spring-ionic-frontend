492a7e39c37da96f7642d4c5da9113e7b7e7552c
Mariana <mxpixel@yahoo.com.br>
2020-02-27 12:21:53 -0300

    Tratando erros 403

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { StorageService } from "../storage.service";
 import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";

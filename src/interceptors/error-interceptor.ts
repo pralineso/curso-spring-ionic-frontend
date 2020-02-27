b6f04a567e1ed51185a2b0149e45f9296bdd0327
Mariana <mxpixel@yahoo.com.br>
2020-02-27 16:58:16 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
-import { AlertController } from "ionic-angular";
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from "../models/fieldmessage";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService, public alertControler: AlertController){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
 
     }
 
@@ -37,6 +38,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                     this.handle403();
                     break;
 
+                case 422:
+                    this.handle422(errorObj);
+                    break;
+
                 default:
                     this.handleDefaultError(errorObj);
             }
@@ -50,7 +55,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     handle401(){
-        let alert = this.alertControler.create({
+        let alert = this.alertCtrl.create({
             title: "Erro 401: falha de autenticação",
             message: "Email ou senha incorretos",
             enableBackdropDismiss: false,
@@ -63,8 +68,22 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();//alert nao aparece
+    }
+
     handleDefaultError(errorObj){
-        let alert = this.alertControler.create({
+        let alert = this.alertCtrl.create({
             title: "Erro "+ errorObj.status + ": "+ errorObj.error,
             message: errorObj.message,
             enableBackdropDismiss: false,
@@ -76,6 +95,14 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
         alert.present();
     
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CidadeService } from '../../services/domain/cidade.service';
 import { CidadeDTO } from '../../models/cidade.dto';
 import { EstadoDTO } from '../../models/estado.dto';
 import { EstadoService } from '../../services/domain/estado.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 
 @IonicPage()
@@ -23,23 +24,25 @@ export class SignupPage {
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     public cidadeService: CidadeService,
-    public estadoService: EstadoService) {
+    public estadoService: EstadoService,
+    public clienteSerive: ClienteService,
+    public alertCtrl: AlertController) {
 
 
       this.formGroup = formBuilder.group({
-        nome: ["", [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
-        email: ["", [Validators.required, Validators.email]],
-        tipo: ["", [Validators.required]],
-        cpfOuCnpj: ["", [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        senha: ["", [Validators.required]],
-        logradouro: ["", [Validators.required]],
-        numero: ["", [Validators.required]],
-        complemento: ["",[]],
-        bairro: ["",[]],
-        cep: ["",[Validators.required]],
-        telefone1: ["",[Validators.required]],
-        telefone2: ["",[]],
-        telefone3: ["",[]],
+        nome: ['Mariana', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+        email: ['mari123@gmail.com', [Validators.required, Validators.email]],
+        tipo: ['1', [Validators.required]],
+        cpfOuCnpj: ['14541967774', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha: ['12345', [Validators.required]],
+        logradouro: ['rua das flores', [Validators.required]],
+        numero: ['231', [Validators.required]],
+        complemento: ['',[]],
+        bairro: ['',[]],
+        cep: ['11050050',[Validators.required]],
+        telefone1: ['992458899',[Validators.required]],
+        telefone2: ['',[]],
+        telefone3: ['',[]],
         estadoId: [null, [Validators.required]],
         cidadeId: [null, [Validators.required]]
       });
@@ -66,6 +69,27 @@ export class SignupPage {
   }
 
   signupUser(){
-    console.log('enviou form');
+    this.clienteSerive.insert(this.formGroup.value)
+    .subscribe(response => {
+      this.showInsertOk();
+    },
+    error =>{});
+  }
+
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }

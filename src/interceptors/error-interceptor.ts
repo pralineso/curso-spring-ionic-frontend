0d5d0cf421c35ebae4ca8de77c250189d3b1de70
Mariana <mxpixel@yahoo.com.br>
2020-02-27 11:44:03 -0300

    Interceptor para incluir token nas requisicoes

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -6,19 +6,15 @@ import { Observable } from "rxjs/Rx";
 export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log("Passou no Interceptor");
         return next.handle(req)
         .catch((error, caught) => {
 
             let errorObj = error;
-            //testa q o erroobj veio com campo erro do httperroresponse
-            //ai se veio vai pega/guardar so o campo error, pq é o q tem praticamente td q ja tem la tratado do backend
+            
             if (errorObj.error){
                 errorObj = errorObj.error;
             }
 
-            //caso a resposta venha no tipo texto ai converte pra json7
-            //por isos q verifica se o camo erro veio com status, pq se veio..é certo q ja em json, mas senao.. converte
             if(!errorObj.status){
                 errorObj = JSON.parse(errorObj);
             }
